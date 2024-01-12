6e1cc1ce66a7ba5a7609ce5515d309e53478426a
Chrislain GBA <gbachrislain@gmail.com>
2024-01-12 19:59:47 +0100

    commit rebase

--- src/com/xl1/dp/designpatterns/strategy/StrategyImpl3.java
+++ src/com/xl1/dp/designpatterns/strategy/StrategyImpl3.java
@@ -4,7 +4,7 @@ public class StrategyImpl3 implements Strategy{
 
 	@Override
 	public void operationStrategy() {
-		System.out.println("Application Strategy 3");
+		System.out.println("Application Strategy 33");
 		
 	}
 
